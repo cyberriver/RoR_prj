e0a444ab7425115d82c7a8842c0f8d99683a589c
cyberriver <vblagirev@gmail.com>
2021-11-01 18:58:51 +0300

    Сделал классы Route, Train. Все работает

--- Ruby_test3/trains.rb
+++ Ruby_test3/trains.rb
@@ -32,13 +32,15 @@
 
 class Station #
   @@station_count = 0
-  @@stations = Hash.new(0) #все станции
-  @arrived_trains = Hash.new(0) #прибывшие поезда
+  @@stations =[] #все станции
+
+  attr_accessor :station_name
+  attr_accessor :arrived_trains
 
   def initialize (station_name) #создание станции
     @station_name = station_name
-    @@stations[@station_name]="поездов нет" #создаем конкретную станцию
-    @arrived_trains = {}
+    @@stations.push(@station_name) #создаем конкретную станцию
+    @arrived_trains = []
     @@station_count +=1
     puts "log: создана станция #{@station_name}"
     puts @@stations
@@ -50,17 +52,22 @@ class Station #
 
   def display_all #посмотреть все станции со всеми поездами
     puts "список всех станций"
-    @@stations.each do |key,value|
-      puts "стация #{key}"
+    @i=0
+    @@stations.each do |value|
+      puts "стация №#{@i} #{value}"
+      @i +=1
     end
   end
 
-  def arrival (train_num, train_type) #прибытие поезда на станцию
-    @train_num = train_num
-    @train_type = train_type.to_s
-    @arrived_trains[@train_num] = @train_type
-    @@stations[@station_name] = @arrived_trains
-    puts "log: на станцию #{@station_name} прибыл поезд #{@train_num}"
+  def arrival (train) #прибытие поезда на станцию
+
+    @arrived = train
+    @train_num = @arrived.train_name
+    @train_type = @arrived.train_type
+    @arrived_trains.push(@arrived)
+
+
+    puts "log: на станцию #{@station_name} прибыл поезд #{@arrived_trains.last.train_name}"
     #проверяем что поезда приехали на станцию
 
   end
@@ -82,8 +89,8 @@ class Station #
     @total_train = Hash.new(0)  # итоговый хеш с результатами подсчетов
     i=0
 
-    @arrived_trains.each do |key,value| #каждый тип поезда помещаем в массив для подсчета
-      @count_train[i] =value
+    @arrived_trains.each do |key| #каждый тип поезда помещаем в массив для подсчета
+      @count_train[i] =key.train_type
       i +=1
     end
     puts "список поездов на станции #{@station_name} по типам"
@@ -101,4 +108,143 @@ class Station #
 
   end
 
+end
+
+class Route
+attr_accessor :route_name # имя маршрута
+attr_accessor :begin_station
+attr_accessor :end_station
+@@routes_count = 0
+attr_accessor :route_name
+attr_reader :station_list
+
+  def initialize (name,station1,station2)
+    @route_name = name
+    @station1 = station1
+    @station2 = station2
+    @begin_station = @station1.station_name
+    @end_station = @station2.station_name
+    @station_list = [@begin_station,@end_station]
+    @@routes_count +=1
+    puts "Создан маршрут #{@route_name}"
+    puts "начальная станция: #{@begin_station}, конечная станция: #{@end_station} "
+  end
+
+  def add_station(station) #Добавляем промежуточную станцию, при этом последняя всегда сдвигается
+    @station_new = station
+    @last_station = @station_list.last
+    @station_list[@station_list.length-1] = @station_new.station_name
+    @station_list.insert(@station_list.length, @last_station)
+  end
+  def delete (i)
+    if i <= @station_list.length && i>0
+      @station_list.delete_at(i-1)
+      puts "станция #{i} удалена"
+    else
+      puts "такой станции нет"
+    end
+  end
+  def display #отобразить маршрут станций
+    puts "Маршрут состоит из следующих станций"
+    @i = 0
+    @station_list.each do |value|
+      puts "#{@i+1}. #{@station_list[@i]}"
+      @i +=1
+    end
+
+  end
+end
+
+class Train  # класс создаем поезд
+  attr_accessor :speed
+  attr_accessor :wagoon_count
+  attr_accessor :train_name
+  attr_accessor :train_type
+  attr_reader :current_station
+  attr_reader :current_station_name
+
+  def initialize (num,wagoon)
+    @train_name=num
+    @wagoon_count=wagoon.to_i
+    @speed = 0
+    puts "Пжл-та выберите тип поезда "
+
+    loop do
+      puts "1: грузовой, 2: пассажирский"
+      @train_type = gets.chomp.to_i
+      break if @train_type == 1 || @train_type ==2
+    end
+  end
+
+  def gas_on (delta) #повышение скорости поезда на дельту
+    @speed +=delta
+  end
+
+  def change_wagoon
+
+
+        if @speed != 0
+          puts "поезд находится в движении, выполните команду stop"
+        else
+
+          loop do
+            puts "чтобы отцепить вагон нажмите 1, чтобы прицепить 2"
+            @operation = gets.chomp.to_i
+            if @operation ==1
+              @wagoon_count = @wagoon_count -1
+            elsif @operation ==2
+              @wagon_count = @wagoon_count + 1
+            else
+              puts "выбрана некорректная операция. Выберите снова"
+            end
+            break if @operation ==1 || @operation ==2
+          end
+        end
+
+
+  end
+
+  def stop #полный стоп
+    @speed = 0
+  end
+
+  def set_route (route) #назначить маршрут
+    @route = route
+    puts "назначен маршрут #{@route.route_name}"
+    puts @route.begin_station
+    @current_station = 0
+    @current_station_name =@route.station_list[0]
+
+  end
+
+  def go_on_route
+    puts "Поезд отправляется со станции #{@route.station_list[@current_station]}"
+
+    if @current_station <@route.station_list.count-1
+      @current_station +=1
+      @current_station_name = @route.station_list[@current_station]
+      puts "Вы прибыли на станцию#{@current_station_name}"
+    else
+      puts "Вы достигли конечной станции. Поезд дальше не идет"
+    end
+  end
+
+  def go_back_route
+    puts "Поезд отправляется со станции #{@route.station_list[@current_station]}"
+
+    if @current_station <@route.station_list.count &&@current_station>0
+      @current_station -=1
+      @current_station_name = @route.station_list[@current_station]
+      puts "Вы прибыли на станцию #{@current_station_name}"
+    else
+      puts "Вы достигли конечной станции. Поезд дальше не идет"
+    end
+  end
+
+  def display_st
+    puts "текущая станция #{@route.station_list[@current_station]}"
+    puts "предыдущая станция #{@route.station_list[@current_station-1]}"
+    puts "следующая станция #{@route.station_list[@current_station]+1}"
+
+  end
 end
