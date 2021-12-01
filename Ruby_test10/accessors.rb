0adb83d952cacbe04cc7eb384ae291dd80bcd250
cyberriver <vblagirev@gmail.com>
2021-12-01 16:41:14 +0300

    some progress

--- Ruby_test10/accessors.rb
+++ Ruby_test10/accessors.rb
@@ -1,7 +1,7 @@
 module Accessors
   def attr_accessor_with_history(*methods)
     methods.each do |method|
-      method_name = "#{method}".to_sym
+      method_name = "@#{method}".to_sym
       define_method(method) {instance_variable_get(method_name)}
       define_method("#{method}=".to_sym) { |value| instance_variable_set(method_name, value) }
     end
